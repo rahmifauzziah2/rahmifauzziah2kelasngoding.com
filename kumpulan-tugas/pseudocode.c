cb94e86fa6ee165daec48d88e9c32d01108807b7
rahmifauzziah2 <rahmifauzziah2@gmail.com>
2026-06-11 13:58:44 +0700

    Create pseudocode.c

---
+++ kumpulan-tugas/pseudocode.c
@@ -0,0 +1,15 @@
+PSEUDOCODE
+
+Algoritma Menentukan_Kelulusan
+(Menetukan nilai kelulusan termasuk Anda Lulus atau Anda Gagal)
+
+Kamus
+n : integer
+
+Deskripsi
+read(nilai)
+if nilai > 50 then
+write(“Anda Lulus”)
+else
+write(“Anda Gagal”_
+endif
